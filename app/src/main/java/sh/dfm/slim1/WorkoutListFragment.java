4770b0c9e977713d5b3a8be8b672038adb70314c
Beletskiy Dmitrii <legioner009@gmail.com>
2016-12-04 11:07:29 +0200

    2 fragment

--- app/src/main/java/sh/dfm/slim1/WorkoutListFragment.java
+++ app/src/main/java/sh/dfm/slim1/WorkoutListFragment.java
@@ -7,29 +7,41 @@ import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class WorkoutListFragment  extends ListFragment{
 
-    private WorkoutListListner mainAcrivityReference;
+//    private WorkoutListListner mainAcrivityReference;
 
-    public static interface WorkoutListListner {
-        void itemClicked(long id);
-    }
-
-    @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-        this.mainAcrivityReference =(WorkoutListListner) activity;
-    }
+//    public static interface WorkoutListListner {
+//        void itemClicked(long id);
+//    }
+//
+//    @Override
+//    public void onAttach(Activity activity) {
+//        super.onAttach(activity);
+//        this.mainAcrivityReference =(WorkoutListListner) activity;
+//    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         String[] names = new String[Workout.workouts.length];
         for (int i = 0; i < names.length; i++) {
-//            names[i] = W
+            names[i] = Workout.workouts[i].getName();
         }
+        //ArrayAdapter<String> adapter = new ArrayAdapter<String>(inflater.getContext(),
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_list_item_1,names);
+        setListAdapter(adapter);
 
         return super.onCreateView(inflater, container, savedInstanceState);
     }
+
+//    @Override
+//    public void onListItemClick(ListView l, View v, int position, long id) {
+//        //super.onListItemClick(l, v, position, id);
+//        mainAcrivityReference.itemClicked(id);
+//    }
 }
